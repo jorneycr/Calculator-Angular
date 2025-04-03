e262357aab4aa3749bd9e5199a99da9ba4112481
Jorney Lopez <jorneycr@hotmail.com>
2025-04-03 08:51:20 -0600

    create more components

--- src/app/calculadora/calculadora.component.ts
+++ src/app/calculadora/calculadora.component.ts
@@ -1,20 +1,27 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { FormularioComponent } from "../formulario/formulario.component";
+import { ResultadoComponent } from "../resultado/resultado.component";
 
 @Component({
   selector: 'app-calculadora',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, FormularioComponent, ResultadoComponent],
   templateUrl: './calculadora.component.html',
   styleUrl: './calculadora.component.css'
 })
 export class CalculadoraComponent {
+
   operandoA: number = 0;
   operandoB: number = 0;
-  resultado: number = 0;
+  resultadoPadre: number = 0;
 
   sumar() {
-    this.resultado = this.operandoA + this.operandoB;
+    this.resultadoPadre = this.operandoA + this.operandoB;
   }
+
+  procesarResultado(resultadoHijo: number) {
+    this.resultadoPadre = resultadoHijo;
+    }
 
 }
